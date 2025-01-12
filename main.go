b935f4ca243c5a5e76e48c7c626655772b55fa50
Andre Abreu <andre.abreu@gmail.com>
2025-01-12 12:33:51 -0300

    :recycle: chore(main) Update CLI tool messaging and AI model integration
    - Enhanced the short description of the CLI tool to include commit message generation functionality when Git changes are detected.
    - Removed redundant newlines in the CLI output for a cleaner presentation of suggested commit messages.
    - Switched AI model in API calls from "gpt-4" to "gpt-4o" to utilize updated model capabilities.
    - Added function `parseBranchForCommitType` to dynamically determine commit type and context from the Git branch name for generating commit messages.
    - Refined the prompt construction logic in the `generateCommitMessage` function to better align with Git flow principles.

--- main.go
+++ main.go
@@ -23,8 +23,9 @@ func main() {
 	LoadEnv()
 
 	var rootCmd = &cobra.Command{
-		Use:     "gojira",
-		Short:   "Uma ferramenta CLI de uso interno para criar descrições de tarefas para o Jira",
+		Use: "gojira",
+		Short: "Uma ferramenta CLI de uso interno para criar descrições de tarefas para o Jira ou mensagens de commit " +
+			"se estiver em um repositório Git e houver arquivos modificados",
 		Version: version,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			title, _ := cmd.Flags().GetString("title")
@@ -57,7 +58,7 @@ func main() {
 					if err != nil {
 						return err
 					}
-					fmt.Println("\nMensagem de commit sugerida:\n")
+					fmt.Println("\nMensagem de commit sugerida:")
 					fmt.Println(commitMessage)
 				}
 				return nil
@@ -153,7 +154,7 @@ func CallOpenAI(prompt string) (string, error) {
 
 	url := "https://api.openai.com/v1/chat/completions"
 	body := map[string]interface{}{
-		"model":    "gpt-4",
+		"model":    "gpt-4o",
 		"messages": []map[string]string{{"role": "user", "content": prompt}},
 	}
 
@@ -280,12 +281,33 @@ func getGitDiff() (map[string]string, error) {
 	return diffs, nil
 }
 
+//goland:noinspection GoPrintFunctions
 func generateCommitMessage(diffs map[string]string, branch string) (string, error) {
-	prompt := "Você é um assistente de IA. Analise os seguintes diffs do Git e, considerando o contexto da branch atual " +
-		"e as regras do Git Flow, forneça uma mensagem de commit objetiva, clara e sucinta em inglês dos EUA, utilizando " +
-		"gitemoji que reflita a tarefa somente no titulo. Certifique-se de que a mensagem:\n- Reflita as alterações feitas.\n" +
-		"- Alinhe-se com o propósito da branch (por exemplo, feature, bugfix, hotfix, etc.).\n- Use convenções padrão do " +
-		"Git Flow para mensagens de commit.:\n\n"
+	commitType, context, err := parseBranchForCommitType(branch)
+	if err != nil {
+		return "", err
+	}
+
+	prompt := fmt.Sprintf(
+		"Você é um assistente de IA. Analise os seguintes diffs do Git e, considerando o contexto da branch atual "+
+			"e as regras do Git Flow, forneça uma mensagem de commit no seguinte formato, **sem introduções ou explicações adicionais**:\n\n"+
+			" %s(%s) Refactor project structure for better modularity\n\n"+
+			"- Item 1: Descrição breve e clara do que foi alterado ou adicionado.\n"+
+			"- Item 2: Descrição breve e clara de outra alteração ou melhoria.\n"+
+			"- ... (adicione mais itens conforme necessário para refletir as mudanças).\n\n"+
+			"Certifique-se de que:\n"+
+			"- É imprencidivel o uso de gitemoji no titulo do commit de acordo com o tipo de commit ou que reflita o contexto.\n"+
+			"- Utilize prioritariamente o gitflow e o padrão de commits do projeto.\n"+
+			"- O título seja objetivo e resuma a essência das alterações.\n"+
+			"- Os itens mencionem os novos arquivos criados (se houver) e suas funções, sem se limitar a extensões específicas.\n"+
+			"- A mensagem seja clara e reflita as mudanças de forma concisa.\n\n"+
+			"Por exemplo:\n\n"+
+			":gitemoji: commit_type(branch_name) Refactor project structure for better modularity\n\n"+
+			"- Created new files to separate concerns and improve organization.\n"+
+			"- Modularized the codebase by introducing utility and service layers.\n"+
+			"- Enhanced main entry point to integrate the refactored structure.\n\n"+
+			"Responda **preferencialmente** no formato acima.",
+		"{{commit objective like feat, fix, chore}}", commitType, context)
 	for file, diff := range diffs {
 		prompt += fmt.Sprintf("Branch: %s\nFile: %s\nChanges:\n%s\n\n", branch, file, diff)
 	}
@@ -329,4 +351,18 @@ func isIgnored(file string, ignoredPatterns []string) bool {
 		}
 	}
 	return false
+}
+
+func parseBranchForCommitType(branch string) (string, string, error) {
+	parts := strings.Split(branch, "/")
+	if len(parts) < 2 {
+		commitType := "hotfix"
+		context := branch
+		return commitType, context, nil
+	}
+
+	commitType := parts[0]
+	context := strings.Join(parts[1:], "/")
+
+	return commitType, context, nil
 }
